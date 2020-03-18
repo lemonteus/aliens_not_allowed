4574acb3a0c088d9da3c83798377d7e110f72fc0
Pedro <holo_vaz@outlook.com>
2020-03-18 16:02:37 -0300

    defined main's pressedKey, drawScene and reshape

--- src/main.c
+++ src/main.c
@@ -1,3 +1,5 @@
+#include <stdbool.h>
+
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
@@ -7,8 +9,7 @@
 #include "./view/game.h"
 #include "./view/afterGame.h"
 
-enum screen { intro, game, afterGame };
-
+enum basicStructures_screen main_screenDef = intro;
 
 void initialize();
 void drawScene();
@@ -32,4 +33,55 @@ int main(int argc, char **argv){
     glutMainLoop();
 
     return 0;
+}
+
+void initialize(){
+    glClearColor(1, 1, 1, 1);
+    main_screenDef = intro;
+}
+
+void drawScene(){
+    switch (main_screenDef){
+    case intro:
+        intro_drawScene();
+        break;
+    case game:
+        game_drawScene();
+        break;
+    case afterGame:
+        afterGame_drawScene(game_finalStatus);
+        break;
+    default:
+        break;
+    }
+    glutSwapBuffers();
+}
+
+void reshape(int width, int height){
+    // (x, y, width, height)
+    glViewport(0, 0, width, height);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    // (left, right, bottom, top, zNear, zFar)
+    glOrtho(-(width/2), width/2, -(height/2), height/2, -1, 1);
+
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();
+}
+
+void pressedKey(unsigned char key, int x, int y){
+    switch (main_screenDef){
+    case intro:
+        intro_pressedKey(key, x, y);
+        break;
+    case game:
+        game_pressedKey(key, x, y);
+        break;
+    case afterGame:
+        afterGame_pressedKey(key, x, y);
+        break;
+    default:
+        break;
+    }
 }
